2658a616ec183d70e859388bcb2b956afaa8ca43
shshwdr <cs.vivian04@gmail.com>
2023-05-20 11:42:47 -0700

    minor

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -18,6 +18,9 @@ public class GameManager : Singleton<GameManager>
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            restartGame();
+        }
     }
 }
