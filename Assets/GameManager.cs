dcbe857ce4214b4acae163025ba992a2a6a91771
shshwdr <cs.vivian04@gmail.com>
2023-05-20 18:44:04 -0700

    add flowers

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
+    public  System.Random random;
+    private int seed = 0;
     public void restartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+         random = new System.Random(seed);
     }
 
     // Update is called once per frame
--- Assets/Ground.cs
+++ Assets/Ground.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
+    public List<GameObject> flower;
+
+    public Transform flowerPositionParent;
+    Transform[] flowerPositions;
+    private int maxPlant = 4;
+    private HashSet<int> visited = new HashSet<int>();
     // Start is called before the first frame update
     void Start()
     {
+        flowerPositions = flowerPositionParent.GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < maxPlant; i++)
+        {
+            if (!create())
+            {
+                break;
+            }
+        }
+    }
+
+    bool create()
+    {
         
+        var rand1 = GameManager.Instance.random.Next(flowerPositions.Length);
+        var rand2 = GameManager.Instance.random.Next(flower.Count);
+if(visited.Contains(rand1))
+{
+    return false;
+}
+
+visited.Add(rand1);
+        Instantiate(flower[rand2], flowerPositions[rand1]);
+        return true;
     }
 
     // Update is called once per frame
